abaa2bb2b93648511ab94b67fc173bf3bf94f622
Daddyjohn63 <john@webandprosper.co.uk>
2025-07-15 20:39:34 +0100

    added remove meeting procedure

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -25,6 +25,25 @@ import { MeetingStatus } from '../types';
 //import { TRPCError } from '@trpc/server';
 
 export const meetingsRouter = createTRPCRouter({
+  remove: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const [removedMeeting] = await db
+        .delete(meetings)
+        .where(
+          and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+        )
+        .returning();
+
+      if (!removedMeeting) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'Meeting not found'
+        });
+      }
+
+      return removedMeeting;
+    }),
   update: protectedProcedure
     .input(meetingsUpdateSchema)
     .mutation(async ({ ctx, input }) => {
@@ -44,6 +63,7 @@ export const meetingsRouter = createTRPCRouter({
       }
       return updatedMeeting;
     }),
+
   create: protectedProcedure
     .input(meetingsInsertSchema)
     .mutation(async ({ input, ctx }) => {
