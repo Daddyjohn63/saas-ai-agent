b361da21917032beca98ab0edd370dd52d68f008
Daddyjohn63 <john@webandprosper.co.uk>
2025-07-16 14:24:25 +0100

    Meeting variants and variant states added

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -14,6 +14,11 @@ import { ErrorState } from '@/components/error-state';
 import { LoadingState } from '@/components/loading-state';
 import { MeetingIdViewHeader } from '../components/meeting-id-view-header';
 import { UpdateMeetingDialog } from '../components/update-meeting-dialog';
+import { UpcomingState } from '../components/upcoming-state';
+import { CancelledState } from '../components/cancelled-state';
+import { ProcessingState } from '../components/processing-state';
+import { CompletedState } from '../components/completed-state';
+import { ActiveState } from '../components/active-state';
 
 // import { ActiveState } from "../components/active-state";
 // import { UpcomingState } from "../components/upcoming-state";
@@ -65,11 +70,11 @@ export const MeetingIdView = ({ meetingId }: Props) => {
     await removeMeeting.mutateAsync({ id: meetingId });
   };
 
-  // const isActive = data.status === "active";
-  // const isUpcoming = data.status === "upcoming";
-  // const isCancelled = data.status === "cancelled";
-  // const isCompleted = data.status === "completed";
-  // const isProcessing = data.status === "processing";
+  const isActive = data.status === 'active';
+  const isUpcoming = data.status === 'upcoming';
+  const isCancelled = data.status === 'cancelled';
+  const isCompleted = data.status === 'completed';
+  const isProcessing = data.status === 'processing';
 
   return (
     <>
@@ -86,16 +91,18 @@ export const MeetingIdView = ({ meetingId }: Props) => {
           onEdit={() => setUpdateMeetingDialogOpen(true)}
           onRemove={handleRemoveMeeting}
         />
-        {JSON.stringify(data, null, 2)}
-        {/* {isCancelled && <CancelledState />}
+        {/* {JSON.stringify(data, null, 2)} */}
+        {isCancelled && <CancelledState />}
         {isProcessing && <ProcessingState />}
         {isCompleted && <CompletedState data={data} />}
         {isActive && <ActiveState meetingId={meetingId} />}
         {isUpcoming && (
           <UpcomingState
             meetingId={meetingId}
+            onCancelMeeting={() => {}}
+            isCancelling={false}
           />
-        )} */}
+        )}
       </div>
     </>
   );
